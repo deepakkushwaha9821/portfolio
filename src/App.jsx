b43e01088a1e15a67cb0327009e5d38bf2f8ff16
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2025-08-24 20:47:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const siteProps = {
   email: "deekus1140@gmail.com",
   gitHub: "https://github.com/deepakkushwaha9821",
   instagram: "deepkhushwa",
-  linkedIn: "www.linkedin.com/in/deepak-kushwaha-7875a5252",
+  linkedIn: www.linkedin.com/in/deepak-kushwaha-7875a5252,
   youTube: "@deekusk6340",
 };
 

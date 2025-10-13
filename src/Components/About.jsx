047b44ad3bd87e9f6a7e3a2a86f170f12162d4c9
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2025-10-13 21:52:27 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,8 +12,8 @@ const skillsList = [
   "Bootstrap",
   "Node.js, Express",
   "MongoDB, SQL",
-  "C++ Basics",
-  "Jquery",
+  "C++ ","machine learning"
+  "NLP","Django"
   "Python","Flask"
 ];
 
@@ -42,5 +42,6 @@ const About = () => {
 };
 
 export default About;
+
 
 
